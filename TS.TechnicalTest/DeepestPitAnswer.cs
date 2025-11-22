a8d7b76eaa4e8fa0d04a41f29973c9d84c489aac
chief <ancientgrov@gmail.com>
2025-11-22 12:04:49 +0200

    *add second Test test *add the second solution

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -4,6 +4,62 @@ public class DeepestPitAnswer
 {
     public static int Solution(int[] points)
     {
-        throw new NotImplementedException("Not completed yet");
+        if (points == null || points.Length < 3)
+        {
+            // We use three dimentions
+            return -1;
+        }
+
+        int maxDepth = 0;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            // Find the left boundary (a "hill" or higher point)
+            if(points[i] <= 0)
+            {
+                continue;
+            }
+            int leftMax = points[i];
+            
+            // If is Increasing then cut
+            if (i + 1 < points.Length)
+            {
+                if (points[i + 1] > leftMax)
+                {
+                    continue;
+                }
+            }
+            
+            int rightMax = 0;
+            List<int> pointsInRange = [];
+            bool crossGround = false;
+
+            for (int r = i; r < points.Length; r++)
+            {
+                pointsInRange.Add(points[r]);
+                if (points[r] > 0 && r != i && points[r]> rightMax && points[r] != rightMax)
+                {
+                    rightMax = points[r];
+                    if (crossGround)
+                    {
+                        break;
+                    }
+                }
+                if (points[r] == 0)
+                {
+                    crossGround = true;
+                }
+            }
+
+            int lowestArea = pointsInRange.Min();
+
+            // Calculate the potential depth at the current point
+            int currentDepth = Math.Min((leftMax-(lowestArea)),(rightMax-(lowestArea)));
+
+            // Update the maximum depth found so far
+            maxDepth = Math.Max(maxDepth, currentDepth);
+        }
+
+        return maxDepth;
     }
 }
